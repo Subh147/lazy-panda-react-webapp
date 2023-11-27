664a49f02fc8c6c9bf9ee89557431f42e3b68709
Subh147 <subhajitmajumder147@gmail.com>
2023-11-27 11:38:32 +0530

    ok

---
+++ src/pages/home.js
@@ -0,0 +1,261 @@
+import React from 'react';
+class Home extends React.Component{
+    render(){
+        return(
+        <div>
+        <header>
+         <a href="#" class="logo">
+         <img src="assets/image/Lazy_panda_font-removebg-preview.png" alt=""/>
+          </a>
+         <ul class="navmenu">
+             <li><a href="#">Home</a></li>
+             <li><a href="#">Shop</a></li>
+             <li><a href="#">Products</a></li>
+             <li><a href="#">Page</a></li>
+            <li><a href="#">Contact</a></li>
+         </ul>
+     <div class="nav_icon">
+         <a href="#"><i class='bx bx-search'></i></a>
+         <a href="#"><i class='bx bx-user'></i></a>
+         <a href="#"><i class='bx bx-cart-alt'></i></a>
+         
+         <div class="bx bx-menu" id="menu-icon"></div>
+     </div>
+     
+ </header>
+
+ <section class="main-home">
+     <div class="main-text">
+         <h4>Casual Collection</h4>
+         <h1>New <br/> Stylish Oversized <br/> Collection</h1>
+         <p> Grab it Now</p>
+
+         <a href="#" class="main-btn"><br/>Shop Now    <i class='bx bx-right-arrow-alt' id="arrow" ></i></a>
+     </div>
+     <div class="down-arrow">
+         <a href="#trending" class="down"><i class='bx bx-down-arrow-alt' ></i></a>
+     </div>
+ </section>
+
+ 
+ <section class="trending-product" id="trending">
+     <div class="center-text">
+         <h2>Our Trending <span/> Products</h2>
+     </div>
+
+     <div class="products">
+         <div class="row">
+             <img src="assets/image/Untitled-1.png" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             <div class="heart-icon">
+                 <i class='bx bx-heart'></i>
+             </div>
+             <div class="ratting">
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star-half'></i>
+             </div>
+             <div class="price">
+                 <h4>Lazy panda Back printed brz2 tshirt</h4>
+                 <p>$45 - $60 </p>
+             </div>
+         </div>
+
+
+         <div class="row">
+             <img src="assets/image/Untitled-2.png" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             <div class="heart-icon">
+                 <i class='bx bx-heart'></i>
+             </div>
+             <div class="ratting">
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star-half'></i>
+             </div>
+             <div class="price">
+                 <h4>LP Original: f7 T-shirt</h4>
+                 <p>$55 - $70 </p>
+             </div>
+         </div>
+
+
+         <div class="row">
+             <img src="assets/image/Untitled-3.png" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             <div class="heart-icon">
+                 <i class='bx bx-heart'></i>
+             </div>
+             <div class="ratting">
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star-half'></i>
+             </div>
+             <div class="price">
+                 <h4>TSS Originals: Dope</h4>
+                 <p>$45 - $60 </p>
+             </div>
+         </div>
+
+
+
+         <div class="row">
+             <img src="assets/image/marvel hardik.png" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             <div class="heart-icon">
+                 <i class='bx bx-heart'></i>
+             </div>
+             <div class="ratting">
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star'></i>
+                 <i class='bx bxs-star-half'></i>
+             </div>
+             <div class="price">
+                 <h4>Marvel classic Logo</h4>
+                 <p>$145 - $160 </p>
+             </div>
+         </div>
+
+     </div>
+ </section>
+ <section class="description" id="des">
+     <div class="center-text2">
+         <h2>Descri<span/>ption</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto aperiam vel, fugiat natus nam quam iusto libero. Dolores harum odit temporibus laudantium est, molestiae, tempore veniam rem qui, quisquam ducimus facilis quia sequi neque dolorem? Laborum cumque, eaque repellendus harum cupiditate, sunt soluta fugiat ut veniam earum error a explicabo culpa illum repudiandae corporis ipsam consectetur? Quae commodi deleniti corrupti, facere quo blanditiis neque consequuntur laudantium reiciendis unde eum laboriosam, deserunt possimus dolore esse velit fugit debitis, natus quibusdam fugiat voluptas provident exercitationem eveniet dolorum. Est temporibus molestiae doloremque qui nobis error nemo dignissimos vero, unde consequuntur beatae! Maxime, eos! Illo excepturi ullam tempora sapiente, impedit doloribus  </p>
+     </div>
+
+ </section>
+
+
+ <section class="catagory" id="cata">
+     <div class="center-text3">
+         <h2>Catagory<span/>'s</h2>
+     </div>
+
+     <div class="products">
+         <div class="row">
+             <img src="assets/image/oversized tshirts.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+
+
+         <div class="row">
+             <img src="assets/image/OVERSIZED_HOODIES_.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+
+
+         <div class="row">
+             <img src="assets/image/SHAKETS_1.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+
+
+
+         <div class="row">
+             <img src="assets/image/shirts_1_4jLyS07.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+
+
+         
+         <div class="row">
+             <img src="assets/image/sweatshirts_3_.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+        
+        
+         <div class="row">
+             <img src="assets/image/jeans_ocmfisK.webp" alt=""/>
+             <div class="product-text">
+                 <h5> Trending</h5>
+             </div>
+             
+         </div>
+
+     </div>
+ </section>
+
+ <section class="contact">
+     <div class="contact-info">
+         <div class="first-info">
+             
+             <img src="assets/image/Lazy_panda_font-removebg-preview.png"  alt=""/>                <p>46 Aveniue Saltlake Sector V <br/> kolkata-700099</p>
+             <p>8420205555</p>
+             <p>subhajitmajumder143@gmail.com</p>
+         </div>
+
+         <div class="social-icon">
+             <a href="#"><i class='bx bxl-facebook-circle'></i></a>
+             <a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+             <a href="#"><i class='bx bxl-twitter' ></i></a>
+             <a href="#"><i class='bx bxl-youtube'></i></a>
+             <a href="#"><i class='bx bxl-linkedin-square'></i></a>
+         </div>
+
+         <div class="second-info">
+             <h4>Support</h4>
+             <p>Contact us</p>
+             <p>About page</p>
+             <p>Shopping </p>
+             <p>privacy</p>
+         </div>
+         
+         <div class="third-info">
+             <h4>Shop</h4>
+             <p>Men's Shopping</p>
+             <p>Women's Shopping</p>
+             <p>Kid's Shopping</p>
+             <p>Discount</p>
+         </div>
+
+         <div class="fourth-info">
+             <h4>Company</h4>
+             <p>About</p>
+             <p>Blog</p>
+             <p>Affilate</p>
+             <p>Login</p>
+         </div>
+
+         <div class="five">
+             <h4>Subscribe</h4>
+             <p> Lore aliquam possimus cupiditatium corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam rem accusantium dignissimos quae repellendus corporis quia eligendi, in odio amet.</p>
+         </div>
+     </div>
+ </section>
+        </div>
+        );
+    }
+}
+export default Home;
